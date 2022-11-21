76f8541e2081418c5748247f66e8487c31304ecf
Haofan Zheng <hzheng6@ucsc.edu>
2022-11-21 00:04:05 -0800

    Added macro to add /MT and /MTd to MSVC builds

--- cmake/mbedTLScpp_UseMbedtlsHeaders.cmake
+++ cmake/mbedTLScpp_UseMbedtlsHeaders.cmake
@@ -34,3 +34,25 @@ macro(mbedTLScpp_UseMbedtlsHeaders_Enclave arg_mbedtls_target)
 			MBEDTLS_CONFIG_FILE="mbedtlscpp_config.h"
 	)
 endmacro()
+
+macro(mbedTLScpp_UseStaticRuntimeLib arg_mbedtls_target)
+	if (MSVC)
+		target_compile_options(${arg_mbedtls_target}
+			PRIVATE
+				$<$<CONFIG:Debug>:/MTd>
+				$<$<CONFIG:RelWithDebInfo>:/MTd>
+				$<$<CONFIG:Release>:/MT>
+				$<$<CONFIG:MinSizeRel>:/MT>
+		)
+	endif()
+endmacro()
+
+macro(mbedTLScpp_Decentize_Normal arg_mbedtls_target)
+	mbedTLScpp_UseStaticRuntimeLib(${arg_mbedtls_target})
+	mbedTLScpp_UseMbedtlsHeaders_Normal(${arg_mbedtls_target})
+endmacro()
+
+macro(mbedTLScpp_Decentize_Enclave arg_mbedtls_target)
+	mbedTLScpp_UseStaticRuntimeLib(${arg_mbedtls_target})
+	mbedTLScpp_UseMbedtlsHeaders_Enclave(${arg_mbedtls_target})
+endmacro()
